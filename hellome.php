822358d40c3affd4829e3ab1676881bc3fdddd84
Eric Miller <em0194192@otc.edu>
2024-08-27 12:17:28 -0500

    Added Hellome.php

---
+++ hellome.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Name: Eric Miller
+Class: CIS-239
+Date: 8/27/2024
+*/
+
+$name = "Eric";
+
+$student_details = [
+    'degree' => 'CS',
+    'mode_of_transportation' => 'Car',
+    'github_link' => 'https://github.com/em0194192/phpvariables',
+];
+
+echo $name . "\n";
+
+print_r($student_details);
