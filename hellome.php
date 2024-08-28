568ca02048001ecbf6396ce6dd9cd01c2092517c
Eric Miller <em0194192@otc.edu>
2024-08-27 21:12:47 -0500

    Finished hellome

--- hellome.php
+++ hellome.php
@@ -5,14 +5,18 @@ Class: CIS-239
 Date: 8/27/2024
 */
 
+//create a variable that stores my name
 $name = "Eric";
 
+//Create and populate the associate array
 $student_details = [
     'degree' => 'CS',
     'mode_of_transportation' => 'Car',
     'github_link' => 'https://github.com/em0194192/phpvariables',
 ];
 
-echo $name . "\n";
+//print my name
+echo $name . "\n"; 
 
-print_r($student_details);
+//print the array
+print_r($student_details); 
